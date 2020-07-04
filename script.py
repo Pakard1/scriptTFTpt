86d0644f25b534dfc2c9b45602ec842d26e78433
Pakard1 <onun.nuno@gmail.com>
2020-07-04 06:53:47 +0100

    clicareuw

--- script.py
+++ script.py
@@ -1,19 +1,23 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)  # Optional argument, if not specified will search path.
-driver.get('http://www.lolchess.gg/')
+driver.get('https://lolchess.gg')
 
 print(driver.title)
 
 
-""" time.sleep(5) # Let the user actually see something!
+select = driver.find_element_by_xpath(".//button[@data-toggle='dropdown']") ### XPATH = .//button[@data-toggle='dropdown']
+select.click()
 
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
+search = driver.find_element_by_name("name")
+search.send_keys("ArtourBabaevsky")
+search.send_keys(Keys.RETURN)
+#search.submit() """
 
 time.sleep(5) # Let the user actually see something!
 
-driver.quit() """
+driver.quit() 
