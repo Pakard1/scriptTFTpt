a3697352f29ff156148182adaec9e8fa0c773384
Pakard1 <onun.nuno@gmail.com>
2020-07-04 22:30:41 +0100

    script acabado

--- script_test.py
+++ script_test.py
@@ -4,14 +4,16 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from tabulate import tabulate
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)  # Optional argument, if not specified will search path.
 
-players = ['ArtourBabaevsky', 'Kuzma is High'] #, 'EGN Rodrigues', 'Nyxer TFT', 'FTW AïM'
+players = ['ArtourBabaevsky', 'EGN Rodrigues', 'Nyxer TFT', 'FTW AïM', 'Kuzma is High', 'Kilnae', 'Jitonce', 'Batata', 'EGN Renygp xD',
+            'Drunkiris', 'levelup_brain']
 
-data = {}
-df = pd.DataFrame(data, columns = ['Position','Player'])
+players_positions = {}
+players_positions = pd.DataFrame(columns = ['Position','Player', 'Tier', 'LP', 'Wins', 'Top4', 'Played', 'Win Rate %', 'Top4 Rate %' ])
 
 for player in players:
     driver.get('https://lolchess.gg/profile/euw/' + player)
@@ -42,32 +44,39 @@ for player in players:
 
     player_winrate = driver.find_element_by_class_name("profile__tier__winrate_10")
     #winrate = [int(s) for s in player_winrate.text.split() if s.isdigit()]
-    winrate = re.findall("\d*\.?\d+",player_winrate.text)
+    winrate = re.findall("\d*\.?\d+", player_winrate.text)
     print(f"Win Rate: {winrate[0]}%")
 
     player_toprate = driver.find_element_by_class_name("profile__tier__toprate")
     #toprate = [int(s) for s in player_toprate.text.split() if s.isdigit()]
-    toprate = re.findall("\d*\.?\d+",player_toprate.text)
+    toprate = re.findall("\d*\.?\d+", player_toprate.text)
     print(f"Top4 Rate: {toprate[1]}%")
 
     player_avg_rank = driver.find_element_by_class_name("profile__tier__avg_rank")
     #avg_rank = [int(s) for s in player_avg_rank.text.split() if s.isdigit()]
-    avg_rank = re.findall("\d*\.?\d+",player_avg_rank.text)
-    print(f"Avg. Rank: #{avg_rank[0]}")
+    avg_rank = re.findall("\d*\.?\d+", player_avg_rank.text)
+    print(f"Avg. Rank: #{avg_rank}")
 
     position_player = player_rank_region.text.replace("#", "")
 
-    #positions.update({player, position_player}) 
+    new_row = {'Position': position_player, 'Player': player, 'Tier': player_tier.text, 
+                'LP': player_lp.text, 'Wins': wins[0], 'Top4': top4[0], 'Played': played[0],
+                'Win Rate %': winrate[0], 'Top4 Rate %': toprate[1] }
+    #players_positions.insert(-1, position_player, player) 
+    players_positions = players_positions.append(new_row, ignore_index=True)
 
-    print(f"{position_player}, {player}")
-
+    print("\n")
     
+    #print(f"mydataframe: {mydataframe}")
+    #print(f"player_positions: {players_positions}")
+    #print(f"{position_player}, {player}")
+
     time.sleep(5) # Let the user actually see something!
 
 #print(positions)
 
 print("-------------------------------------")
 print("\n")
-print(df)
+print(tabulate(players_positions, headers='keys', tablefmt='psql'))
 
 driver.quit() 
